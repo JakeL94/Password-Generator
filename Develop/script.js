6d0516276348b893cb46dad6625c52b4fb27d635
jake <jakedlee2018@gmail.com>
2021-09-08 11:12:37 -0500

    javascript added

---
+++ Develop/script.js
@@ -0,0 +1,92 @@
+// variables for password generation
+const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+const symbols = "!@#$%^&*()=+";
+const numbers = "1234567890";
+
+
+
+// variables for user to choose which elements to include in password
+const checkUpper = document.querySelector('#upper');
+const checkLower = document.querySelector('#lower');
+const checkNumber = document.querySelector('#number');
+const checkSymbol = document.querySelector('#symbol');
+
+
+
+// get random upper letters, lower letters, symbols and numbers
+function lowerCaseGenerate() {
+  return lowerLetters[Math.floor(Math.randoM() * lowerLetters.length)];
+}
+
+function upperCaseGenerate() {
+  return upperLetters[Math.floor(Math.random() * upperLetters.length)];
+}
+
+function symbolGenerate() {
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+function numberGenerate() {
+  return numbers[Math.floor(Math.random() * numbers.length)];
+}
+
+//grab elements if boxes are checked
+function generateElement() {
+  const element = [];
+
+  if(checkLower.checked) {
+    element.push(lowerCaseGenerate())
+  }
+
+  if(checkUpper.checked) {
+    element.push(upperCaseGenerate())
+  }
+
+  if(checkNumber.checked) {
+    element.push(numberGenerate())
+  }
+
+  if(checkSymbol.checked) {
+    element.push(symbolGenerate())
+  }
+
+  return element[Math.floor(Math.random() * element.length)];
+}
+
+//Password generation
+function generatePassword() {
+  const length = document.querySelector('#length');
+
+  let password = "";
+
+  for (let i = 0; i < length; i++) {
+    const element = generateElement();
+    password += element;
+  }
+  return password;
+};
+
+console.log(generatePassword());
+
+// Get references to the #generate element
+const generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  const length = document.querySelector('#length');
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  if(length < 8 || length > 128) {
+    return window.alert('Password must be between 8 and 128 characters!');
+  }
+  else {
+    passwordText.value = password;
+  }  
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
